4cac433dacaf1a3dd4423bda8a5760881c828a0a
michelemarzioni <michelemarzioni@Air-di-Michele.homenet.telecomitalia.it>
2023-01-07 17:40:10 +0100

    pp

--- src/progetto/Pianeta.java
+++ src/progetto/Pianeta.java
@@ -3,26 +3,23 @@ package progetto;
 public class Pianeta {
     private String nomePianeta;
      private int nCitta;
+     private int nRegali;
+     private int costoRegali;
 
     public Pianeta(String n, int c) {
         this.nomePianeta = n;
         this.nCitta = c;
     }
-
-    public void setNomePianeta(String n){
-        this.nomePianeta = n;
-    }
-
-    public void setnCitta(int nCitta) {
-        this.nCitta = nCitta;
-    }
-
     public String getNomePianeta() {
         return nomePianeta;
     }
+    public void setnRegali(int nRegali) { this.nRegali = nRegali; }
 
-    public int getnCitta() {
-        return nCitta;
+    public int getnRegali() { return nRegali; }
+
+    public int getCostoRegali() { return costoRegali; }
+
+    public void setCostoRegali(int costoRegali) {
+        this.costoRegali = costoRegali;
     }
-
 }
--- src/progetto/Progetto.java
+++ src/progetto/Progetto.java
@@ -7,19 +7,24 @@ import java.util.ArrayList;
 
 public class Progetto {
     public static void main(String[] args) throws IOException {
-
-        //TASK 1
         ArrayList<Regalo> regali = new ArrayList<>();
+        ArrayList<String> regaliOrdinati = new ArrayList<>();
         ArrayList<Pianeta> pianeti = new ArrayList<>();
         ArrayList<Citta> arrCitta = new ArrayList<>();
         int regaliTotali = 0;
-        int nCitta = 0;
+        int arte = 0;
+        int sport = 0;
+        int elettronica = 0;
+        int regaliPianeta = 0;
+        int costoPianeta = 0;
+        Pianeta maxPianeta = new Pianeta("" , 0);
+        Pianeta costoRegaliPianeta = new Pianeta(" ", 0);
         Collegamento c;
         ArrayList<String> destinazioni = new ArrayList<>();
         ArrayList<Integer> pesi = new ArrayList<>();
         char primoCarattere;
         char secondoCarattere;
-
+        //lettura file
         BufferedReader reader = new BufferedReader((new FileReader("Esempi_traccia_A/es_input_1.txt")));
         String line;
         while((line = reader.readLine()) != null){
@@ -33,10 +38,9 @@ public class Progetto {
 
             else if(primoCarattere == 'p'){
                 Pianeta pianeta = new Pianeta(splittedArr[0], Integer.parseInt(splittedArr[1]));
-                nCitta += pianeta.getnCitta();
                 pianeti.add(pianeta);
-
             }
+
             else if(primoCarattere == 'c'){
                 secondoCarattere = splittedArr[1].charAt(0);
                 if(secondoCarattere == 'p') {
@@ -57,12 +61,73 @@ public class Progetto {
                     }
                     destinazioni.clear();
                     pesi.clear();
+
+                }
+                else{
+                    regaliTotali += (splittedArr.length) - 2;
+
+                    for(Pianeta p: pianeti){
+                        //controllo se la citta e' nell'array delle citta
+                        for(Citta c1 : arrCitta){
+                            if(c1.getNomeCitta().equals(splittedArr[0])){
+                                if(c1.getPianetaO().equals(p.getNomePianeta())){
+                                    for (int i = 2; i < splittedArr.length; i++) {
+                                        for(Regalo r : regali){
+                                            if(r.getNomeRegalo().equals(splittedArr[i]))
+                                                costoPianeta += r.getCosto();
+                                        }
+                                    }
+                                    System.out.println(costoPianeta);
+                                }
+                            }
+                        }
+                        p.setCostoRegali(costoPianeta);
+                        if(p.getCostoRegali() > costoRegaliPianeta.getCostoRegali()) costoRegaliPianeta = p;
+                    }
+
+                    for (int i = 2; i < splittedArr.length; i++) {
+                        if(!regaliOrdinati.contains(splittedArr[i])) {
+                            regaliOrdinati.add(splittedArr[i]);
+                        }
+                        for(Regalo r : regali){
+                            if(r.getNomeRegalo().equals(splittedArr[i])){
+                                switch (r.getTipo()){
+                                    case "arte":
+                                        arte++;
+                                        break;
+                                    case "elettronica":
+                                        elettronica++;
+                                        break;
+                                    case "sport":
+                                        sport++;
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                    for(Pianeta p: pianeti){
+                        for (Citta c1: arrCitta) {
+                            if(c1.getPianetaO().equals(p.getNomePianeta())){
+                                regaliPianeta += (splittedArr.length) - 2;
+                            }
+                        }
+                        p.setnRegali(regaliPianeta);
+                        if(p.getnRegali() > maxPianeta.getnRegali())
+                            maxPianeta = p;
+                    }
                 }
             }
         }
         reader.close();
-        for(Citta o: arrCitta){
-            o.stampaArchi();
-        }
+        //TASK 1
+        //punto 1
+        System.out.println(regaliTotali + " " + regaliOrdinati.size());
+        //punto 2
+        System.out.println(arte + " " + elettronica + " " + sport);
+        //punto 3
+        System.out.println(maxPianeta.getNomePianeta());
+        //punto 4
+        System.out.println(costoRegaliPianeta.getNomePianeta());
+        //punto 5
     }
 }
